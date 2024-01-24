c8bb34eb2fe9b7d55f04ad1f110f7bce5301fb09
jaiganesh7890 <154997242+jaiganesh7890@users.noreply.github.com>
2024-01-24 10:49:45 +0530

    Add files via upload

---
+++ Implement a C Program to perform Bidirectional.c
@@ -0,0 +1,14 @@
+//Implement a C Program to perform Bidirectional
+
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("The number you entered is: %d\n", num);
+
+    return 0;
+}
